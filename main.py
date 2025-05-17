215be2f18c4641216d68b31b62cc6a6ff32275db
aroxu <30624945+aroxu@users.noreply.github.com>
2025-05-17 22:48:32 +0900

    :tada: Initial Commit

---
+++ main.py
@@ -0,0 +1,46 @@
+import os
+from synology_api.core_active_backup import ActiveBackupBusiness
+from dotenv import load
+
+
+def run_backup(task_ids):
+    """
+    Run a backup task on Synology Active Backup for Business
+
+    Args:
+        task_ids (list): List of task IDs to run
+    """
+    # Load credentials from .env file
+    load()
+
+    # Get credentials from environment variables
+    host = os.getenv('SYNOLOGY_HOST')
+    port = os.getenv('SYNOLOGY_PORT')
+    username = os.getenv('SYNOLOGY_USERNAME')
+    password = os.getenv('SYNOLOGY_PASSWORD')
+
+    # Initialize connection to Synology
+    abb = ActiveBackupBusiness(
+        host,
+        port,
+        username,
+        password,
+        secure=True,
+        cert_verify=False,
+        dsm_version=7,
+        debug=False,
+        otp_code=None
+    )
+
+    # Run the backup task
+    abb.backup_task_run(task_ids=task_ids)
+
+
+def main():
+    # Example task ID - you can modify this or accept input parameters
+    task_ids = [45]
+    run_backup(task_ids)
+
+
+if __name__ == "__main__":
+    main()
